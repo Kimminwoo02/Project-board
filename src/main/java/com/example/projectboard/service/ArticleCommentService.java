c3add049a3a10697a91a4cb0abd153c2a6e45b39
minu <made_power@naver.com>
2023-03-12 15:47:44 +0900

    #20 서비스 로직 테스트 코드 업데이트 및 DTO 재설계 쿼리메소드 재작성
    테스트 코드를 업데이트하고 테스트가 필요로하는 Repository 쿼리 메소드도 작성

---
+++ src/main/java/com/example/projectboard/service/ArticleCommentService.java
@@ -0,0 +1,30 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.DTO.ArticleCommentDto;
+import com.example.projectboard.repository.ArticleCommentRepository;
+import com.example.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class ArticleCommentService {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+@Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComments(long articleId) {
+        return List.of();
+    }
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+    }
+}
