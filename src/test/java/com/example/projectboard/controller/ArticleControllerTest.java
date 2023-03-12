530d7f9befdd69dfab2d0431484821af3b73ef08
minu <made_power@naver.com>
2023-03-12 19:00:43 +0900

    #20 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현 후 데이터 전달 완료

--- src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -1,17 +1,31 @@
 package com.example.projectboard.controller;
 
+import com.example.projectboard.DTO.ArticleWithCommentsDto;
+import com.example.projectboard.DTO.UserAccountDto;
 import com.example.projectboard.config.SecurityConfig;
+import com.example.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static net.bytebuddy.matcher.ElementMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +34,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
+    @MockBean
+    private ArticleService articleService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -29,14 +45,14 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         //given
-
+        given(articleService.searchArticles(eq(null),eq(null), ArgumentMatchers.any(Pageable.class))).willReturn(Page.empty());
         //when&then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"));// 이 이름에 데이터가 존재하는지만 판단.
-
+                .andExpect(model().attributeExists("articles"));
+        then(articleService).should().searchArticles(eq(null),eq(null), ArgumentMatchers.any(Pageable.class));
 
 
     }
@@ -47,15 +63,20 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         //given
-
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
         //when&then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/"+articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))// 이 이름에 데이터가 존재하는지만 판단.
                 .andExpect(model().attributeExists("articleComments"));
+        then(articleService).should().getArticle(articleId);
     }
+
+
+
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
@@ -81,5 +102,38 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles/search-hashtag"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+
+
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "Minu",
+                LocalDateTime.now(),
+                "Minu"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                1L,
+                "Minu",
+                "pw",
+                "Minu@naver.com",
+                "Minutaurus",
+                "Memo",
+                LocalDateTime.now(),
+                "Minu",
+                LocalDateTime.now(),
+                "Minu"
+        );
     }
 }
