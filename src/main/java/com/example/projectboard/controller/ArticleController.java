fbd99cff0429f1178bc03cd3ec31a23add0085e6
minu <made_power@naver.com>
2023-03-14 20:51:08 +0900

    #24 - 페이지네이션 기능을 컨트롤러 (게시판 뷰)에 반영)
    페이징 및 정렬 테스트까지 함께 테스트 설계

--- src/main/java/com/example/projectboard/controller/ArticleController.java
+++ src/main/java/com/example/projectboard/controller/ArticleController.java
@@ -1,11 +1,12 @@
 package com.example.projectboard.controller;
 
-import com.example.projectboard.DTO.response.ArticleCommentResponse;
 import com.example.projectboard.DTO.response.ArticleResponse;
 import com.example.projectboard.DTO.response.ArticleWithCommentResponse;
 import com.example.projectboard.domain.type.SearchType;
 import com.example.projectboard.service.ArticleService;
+import com.example.projectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -24,13 +25,19 @@ import java.util.List;
 @Controller
 public class ArticleController {
     private final ArticleService articleService;
+    private  final PaginationService paginationService;
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort="createdAt",direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map){
-        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumber(pageable.getPageNumber(),articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers",barNumbers);
+
         return "articles/index";
     }
 
